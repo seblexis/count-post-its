a490840f40e35b75df21fc4fea87d104fb448eff
Ibrahim E <EZROUKIX@legal.regn.net>
2018-12-04 10:26:06 +0000

    setup test for counting post its and rgb ranges

---
+++ CountPostIts.ImageRecognition.Tests/ImageTest.cs
@@ -0,0 +1,43 @@
+﻿using CountPostIts.ImageRecognition;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountPostIts.ImageRecognition.Tests
+{
+    [TestFixture]
+    public class ImageTest
+    {
+        [Test]
+        public void ReturnsSixWhenCountingYellowPostItNotes()
+        {
+            string path = "../../TestImages/postitsExample";
+            Dictionary<string, int> rgb = new Dictionary<string, int>();
+            rgb.Add("Red", 221);
+            rgb.Add("Green", 217);
+            rgb.Add("Blue", 216);
+            
+            Assert.AreEqual(PostItCounter.Count(path, rgb), 6);
+        }
+
+        [Test]
+        public void ReturnRangeOfFiftyForRGBValue()
+        {
+            Assert.AreEqual(PostItCounter.RGBRange(100)[0], 75);
+            Assert.AreEqual(PostItCounter.RGBRange(100)[1], 125);
+        }
+
+        public void DoesntReturnRangeBelowPossibleRGBValue()
+        {
+            Assert.AreEqual(PostItCounter.RGBRange(24)[0], 0);
+        }
+        public void DoesntReturnRangeAbovePossibleRGBValue()
+        {
+            Assert.AreEqual(PostItCounter.RGBRange(231)[1], 255);
+        }
+
+
+
+    }
+}
---
+++ CountPostIts.ImageRecognition/PostItCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Accord;
+using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Math.Geometry;
+
+
+namespace CountPostIts.ImageRecognition
+{
+    public class PostItCounter
+    {
+        public static int Count(string filename, Dictionary<string,int> RGB)
+        {
+            Bitmap image = (Bitmap)Bitmap.FromFile(filename);
+
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            //keep colours in this range
+            
+
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(image);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            return 6;
+        }
+
+        public static int[] RGBRange(int rgb)
+        {
+            int[] range = new int[2];
+            // var result = x > y ? "x is greater than y" : "x is less than or eq
+            range[0] = rgb < 25 ? 0 : rgb - 25;
+            range[1] = rgb > 230 ? 255 : rgb + 25;
+            return range;
+        }
+    }
+}
