f8f5c6ae38456610888bf9ff536020f00b10ebc6
CAMPBER1 <CAMPBER1@LNGLONL7034881>
2018-12-07 09:41:36 +0000

    Started building return results class

---
+++ CountPostIts.ConsoleUI.Tests/ReturnResultsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+
+namespace CountPostIts.ConsoleUI.Tests
+{
+    class ReturnResultsTests
+    {
+        private const string Filename = "postits.jpg";
+        private ReturnResults _returnResults;
+
+        [SetUp]
+        public void Setup()
+        {
+            _returnResults = new ReturnResults();
+
+
+
+        }
+
+        [Test]
+        public void Returns_5_when_CountPostits_Is_Called()
+        {
+            _returnResults.PostitResults(Filename);
+           
+        }
+
+
+
+
+    }
+}
