1783c2d8d460a5c81f2fcc0a8793c6ae3bdf9363
sebgrebe <sebbecker@gmx.net>
2018-12-13 10:19:53 +0000

    test for colourranges

---
+++ CountPostIts.ImageRecognition.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountPostIts.ImageRecognition.Tests
+{
+    [TestClass]
+    public class ColourRangesTest
+    {
+        [TestMethod]
+        public void ColourRangesHasRGBProperty()
+        {
+            ColourRanges colourRanges = new ColourRanges();
+            Type typeOfRGB = colourRanges.RGB.GetType();
+            Assert.AreEqual(typeOfRGB, typeof(Dictionary<Colours, int[]>));
+        }
+    }
+
+}
